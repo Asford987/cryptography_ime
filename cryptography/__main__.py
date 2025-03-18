079a3cddea16d53b4f2144685aab4f6655a5ffb3
AlphaTuring01 <fabricio.asfora.001@gmail.com>
2025-03-18 09:01:15 -0300

    starting main file

---
+++ cryptography/__main__.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('args', help='input values for the cryptography module')
+    parser.add_argument('--algorithm', choices=['kyber', 'rsa'], help='the algorithm to use', required=True)
+    parser.add_argument('--public-key', '-p', help='output file name for the public key', type=str, required=True)
+    parser.add_argument('--private-key', '-q', help='output file name for the private key', type=str, required=True)
+    parser.add_argument('--strength', '-s', help='the strength of the algorithm', type=int, default=2)
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    if args.algorithm == 'kyber':
+        from kyber import Kyber
+        kyber = Kyber()
+        kyber.generate_keypair(args.strength)
+        with open(args.public_key, 'wb') as f:
+            f.write(kyber.public_key)
+        with open(args.private_key, 'wb') as f:
+            f.write(kyber.private_key)
+        
+    if args.algorithm == 'rsa':
+        from rsa import RSA
+        rsa = RSA()
+        rsa.generate_keypair(args.strength)
+        with open(args.public_key, 'wb') as f:
+            f.write(rsa.public_key)
+        with open(args.private_key, 'wb') as f:
+            f.write(rsa.private_key)
